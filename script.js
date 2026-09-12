49993109b7eec3fd80ac9d086657c4285eefd5dc
Sannu Kumar Ojha <137702131+SannuKumarOjha@users.noreply.github.com>
2026-09-12 17:28:25 +0530

    Update birthday countdown to October 20

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
     mm = String(today.getMonth() + 1).padStart(2, "0"),
     yyyy = today.getFullYear(),
     nextYear = yyyy + 1,
-    dayMonth = "12/02/",
+    dayMonth = "10/20/",
     birthday = dayMonth + yyyy;
 
   today = mm + "/" + dd + "/" + yyyy;
